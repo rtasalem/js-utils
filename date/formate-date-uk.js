537116490986ede6a3cf04a2991e906106d07b4f
Rana Salem <ranatasalem@gmail.com>
2024-06-26 21:06:11 +0100

    formatDateUk

---
+++ date/formate-date-uk.js
@@ -0,0 +1,10 @@
+const formatDateUK = (dateString) => {
+  const [dd, mm, yyyy] = dateString.split('-')
+  const date = new Date(`${yyyy}-${mm}-${dd}`)
+  const day = date.getDate()
+  const month = date.toLocaleDateString('default', { month: 'long' })
+  const year = date.getFullYear()
+  return `${day} ${month} ${year}`
+}
+
+module.exports = { formatDateUK }
